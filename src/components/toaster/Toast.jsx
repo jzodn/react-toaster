dcb70721707a61028f53f311056cbc8db1401fa1
jzodn <jez.daniel.odonnell@gmail.com>
2024-04-27 12:27:18 +0100

    added animations

--- src/components/toaster/Toast.jsx
+++ src/components/toaster/Toast.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classes from "./Toaster.module.css";
 import { useToast } from '../../hooks/useToast';
 
 export default function Toast({ message, id }) {
   const toast = useToast();
   const timer = useRef(null);
+  const progress = useRef(null);
+  const [dismissed, setDismissed] = useState(false);
 
-  const removeToast = () => {toast.removeToast(id)};
+  const removeToast = () => {
+    setDismissed(true);
+    setTimeout(() => {
+      toast.removeToast(id);
+    }, 400);
+  };
 
   useEffect(() => {
     timer.current = setTimeout(() => {
@@ -18,11 +25,26 @@ export default function Toast({ message, id }) {
     };
   }, []);
 
+  const handleMouseEnter = () => {
+    clearTimeout(timer.current);
+    progress.current.style.animationPlayState = "paused";
+  };
+
+  const handleMouseLeave = () => {
+    const remainingTime = (progress.current.offsetWidth / progress.current.parentElement.offsetWidth) * 5000;
+
+    progress.current.style.animationPlayState = "running";
+
+    timer.current = setTimeout(() => {
+      removeToast()
+    }, remainingTime);
+  }
+
   return (
-    <div className={classes.toast} onClick={() => removeToast()}>
+    <div className={`${classes.toast} ${dismissed ? classes.toastDismissed : ""}`} onClick={() => removeToast()} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <p>{message}</p>
       <div className={classes.toastProgress}>
-        <div className={classes.toastProgressBar}></div>
+        <div ref={progress} className={classes.toastProgressBar}></div>
       </div>
     </div>
   );
