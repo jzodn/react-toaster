3854f48ba80da1da4b098a26030bdb92912b15f8
jzodn <jez.daniel.odonnell@gmail.com>
2024-04-27 15:14:25 +0100

    Removing logs

--- src/reducers/ToastReducer.js
+++ src/reducers/ToastReducer.js
@@ -1,14 +1,12 @@
 export const toastReducer = (state, action) => {
     switch (action.type) {
         case "ADD":
-            console.log(state);
             return{
                 ...state,
                 toasts: [...state.toasts, action.payload]
             };
         case "REMOVE":
             const updatedToasts = state.toasts.filter((toast) => toast.id !== action.payload.id);
-            console.log(state);
             return {
                 ...state,
                 toasts: updatedToasts,
